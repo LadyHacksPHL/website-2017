448002a80a4fcf7c5d3d700b2d7e4a13710851fb
Alex Lash <alexandralash@gmail.com>
2017-03-04 11:18:01 -0500

    Added blank underscores theme

---
+++ wp-content/themes/ladyhacks-2017/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * ladyhacks Theme Customizer
+ *
+ * @package ladyhacks
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function ladyhacks_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'ladyhacks_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function ladyhacks_customize_preview_js() {
+	wp_enqueue_script( 'ladyhacks_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'ladyhacks_customize_preview_js' );
